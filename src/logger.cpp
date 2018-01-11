cc2257e435c0cadc1dd0a41551f19469bbd8dc7a
Billy Zhu <yuxiangz@andrew.cmu.edu>
2018-01-11 09:51:16 -0500

    implement logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "logger.h"
+
 void err_assert(bool value) {
   if (!value) {
-    perror("Fatal Error");
+    perror("fatal");
     exit(0);
   }
 }
 
 void err_assert(bool value, const char *msg) {
   if (!value) {
-    printf("Fatal Error: %s\n", msg);
+    printf("fatal: %s\n", msg);
     exit(0);
   }
+}
+
+/* Logger-related */
+
+namespace Logger {
+
+  line_ending_symbol endl;
+  fatal_logger fatal;
+  info_logger info;
+  debug_logger debug;
+
+  template<>
+  fatal_logger &fatal_logger::operator<<(line_ending_symbol end) {
+    std::cout << std::endl;
+    return *this;
+  }
+
+  template<>
+  info_logger &info_logger::operator<<(line_ending_symbol end) {
+    std::cout << std::endl;
+    return *this;
+  }
+
+  template<>
+  debug_logger &debug_logger::operator<<(line_ending_symbol end) {
+  #ifdef PRINT_DEBUG
+    std::cout << std::endl;
+  #endif
+    return *this;
+  }
+
 }
--- src/logger.h
+++ src/logger.h
@@ -1,9 +1,66 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#define PRINT_DEBUG
+
 #include <iostream>
 
 void err_assert(bool value);
 void err_assert(bool value, const char *msg);
+
+namespace Logger {
+
+  class line_ending_symbol {};
+  
+  struct fatal_logger {
+    fatal_logger() : prefix_used(false) {}
+    void set_prefix(std::string new_prefix) {
+      prefix = std::string(new_prefix);
+    }
+    template<class T>
+    fatal_logger &operator<<(T val) {
+      if (!prefix_used) {
+        prefix_used = true;
+        std::cout << "fatal: " << prefix << std::endl;
+      }
+      std::cout << val;
+      return *this;
+    }
+  private:
+    std::string prefix;
+    bool prefix_used;
+  };
+  struct info_logger {
+    template<class T>
+    info_logger &operator<<(T val) {
+      std::cout << val;
+      return *this;
+    }
+  };
+  struct debug_logger {
+    template<class T>
+    debug_logger &operator<<(T val) {
+    #ifdef PRINT_DEBUG
+      std::cout << val;
+    #endif
+      return *this;
+    }
+  };
+
+  extern line_ending_symbol endl;
+  extern fatal_logger fatal;
+  extern info_logger info;
+  extern debug_logger debug;
+  
+  // define specializations of operator<< for line_ending_symbols
+  template<>
+  fatal_logger &fatal_logger::operator<<(line_ending_symbol end);
+
+  template<>
+  info_logger &info_logger::operator<<(line_ending_symbol end);
+
+  template<>
+  debug_logger &debug_logger::operator<<(line_ending_symbol end);
+}
 
 #endif /* LOGGER_H */
