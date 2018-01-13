f04964d8cee09a708f3dd0ffa74c9269a861d021
Billy Zhu <yuxiangz@andrew.cmu.edu>
2018-01-13 09:35:39 -0500

    implement courses and assessments commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
 
 #include "autolab_client.h"
 #include "client_helpers.h"
@@ -6,27 +9,29 @@
 #include "file_utils.h"
 #include "logger.h"
 
-using namespace std;
-
-const string client_id = "c021c9b12dc597b5b42d783ee285a2bc9a8afcce4a60db5b4b97a1cda551f48d";
-const string client_secret = "5cce1a3f5968308defe8a15d4cb1e47250fc88976ebf9c084155fc86da8050f1";
-const string redirect_uri = "http://localhost:3000/device_flow_auth_cb";
+const std::string client_id = "c021c9b12dc597b5b42d783ee285a2bc9a8afcce4a60db5b4b97a1cda551f48d";
+const std::string client_secret = "5cce1a3f5968308defe8a15d4cb1e47250fc88976ebf9c084155fc86da8050f1";
+const std::string redirect_uri = "http://localhost:3000/device_flow_auth_cb";
 
 AutolabClient ac = AutolabClient(client_id, client_secret, redirect_uri, store_tokens);
 
 void init_autolab_client(AutolabClient &ac) {
-  string at, rt;
+  std::string at, rt;
   load_tokens(at, rt);
   ac.set_tokens(at, rt);
 }
 
 void print_download_help() {
-  cout << "Usage: autolab download [asmt-name-hint]" << endl;
+  Logger::info << "Usage: autolab download [course_name]:[assessment_name]" << Logger::endl;
+}
+
+void print_assessments_help() {
+  Logger::info << "Usage: autolab assessments [course_name]" << Logger::endl;
 }
 
 int perform_device_flow(AutolabClient &ac) {
   Logger::info << "Initiating authorization..." << Logger::endl << Logger::endl;
-  string user_code, verification_uri;
+  std::string user_code, verification_uri;
   ac.device_flow_init(user_code, verification_uri);
   Logger::info << "Please visit " << verification_uri << " and enter the code: " << user_code << Logger::endl;
   Logger::info << "Waiting for user authorization ..." << Logger::endl;
@@ -48,16 +53,27 @@ int perform_device_flow(AutolabClient &ac) {
 
 /* return false if failed to parse */
 bool parse_course_and_asmt(std::string raw_input, std::string &course, std::string &asmt) {
-  string::size_type split_pos = raw_input.find(":");
-  if (split_pos == string::npos) {
+  std::string::size_type split_pos = raw_input.find(":");
+  if (split_pos == std::string::npos) {
     // the entire string is the assessment name.
     // attempt to load course name from config if exists,
     // otherwise report error.
     return false;
   }
   course = raw_input.substr(0, split_pos);
-  asmt = raw_input.substr(split_pos + 1, string::npos);
+  asmt = raw_input.substr(split_pos + 1, std::string::npos);
   return true;
+}
+
+struct namepair {
+  std::string name;
+  std::string display_name;
+  namepair(std::string n, std::string dn) :
+    name(n), display_name(dn) {}
+};
+bool namepair_comparator(const namepair &a, const namepair &b) {
+  if (a.name == b.name) return a.display_name < b.display_name;
+  return a.name < b.name;
 }
 
 /* download assessment into a new directory
@@ -72,7 +88,7 @@ int download_asmt(int argc, char *argv[]) {
   Logger::fatal.set_prefix("Cannot download assessment");
 
   // parse course and assessment name
-  string course_name, asmt_name;
+  std::string course_name, asmt_name;
   if (!parse_course_and_asmt(argv[2], course_name, asmt_name)) {
     Logger::fatal << "Please specify both course and assessment name." << Logger::endl;
     return 0;
@@ -80,9 +96,6 @@ int download_asmt(int argc, char *argv[]) {
 
   Logger::info << "Querying assessment '" << asmt_name << "' of course '" << 
     course_name << "' ..." << Logger::endl;
-
-  // setup AutolabClient
-  init_autolab_client(ac);
 
   // make sure assessment exists
   rapidjson::Document asmt;
@@ -168,8 +181,6 @@ int submit_asmt(int argc, char *argv[]) {
   }
 
   // conflicts resolved, use course_name and asmt_name from now on
-  init_autolab_client(ac);
-
   rapidjson::Document response;
   ac.submit_assessment(response, course_name, asmt_name);
 
@@ -177,10 +188,67 @@ int submit_asmt(int argc, char *argv[]) {
   return 0;
 }
 
+int show_courses(int argc, char *argv[]) {
+  // set up logger
+  Logger::fatal.set_prefix("Cannot get courses");
+
+  rapidjson::Document courses;
+  ac.get_courses(courses);
+  check_error_and_exit(courses);
+
+  Logger::debug << "Found " << courses.Size() << " current courses." << Logger::endl;
+
+  std::vector<namepair> course_list;
+  for (auto &c : courses.GetArray()) {
+    std::string course_name = c.GetObject()["name"].GetString();
+    std::string course_display_name = c.GetObject()["display_name"].GetString();
+    course_list.emplace_back(course_name, course_display_name);
+  }
+  std::sort(course_list.begin(), course_list.end(), namepair_comparator);
+
+  for (auto &c : course_list) {
+    Logger::info << c.name << " (" << c.display_name << ")" << Logger::endl;
+  }
+
+  return 0;
+}
+
+int show_assessments(int argc, char *argv[]) {
+  if (argc < 3) {
+    print_assessments_help();
+    return 0;
+  }
+
+  // set up logger
+  Logger::fatal.set_prefix("Cannot get assessments");
+
+  std::string course_name(argv[2]);
+
+  rapidjson::Document asmts;
+  ac.get_assessments(asmts, course_name);
+  check_error_and_exit(asmts);
+
+  Logger::debug << "Found " << asmts.Size() << " assessments." << Logger::endl;
+
+  std::vector<namepair> asmt_list;
+  for (auto &a : asmts.GetArray()) {
+    std::string asmt_name = a.GetObject()["name"].GetString();
+    std::string asmt_display_name = a.GetObject()["display_name"].GetString();
+    asmt_list.emplace_back(asmt_name, asmt_display_name);
+  }
+  std::sort(asmt_list.begin(), asmt_list.end(), namepair_comparator);
+
+  for (auto &c : asmt_list) {
+    Logger::info << c.name << " (" << c.display_name << ")" << Logger::endl;
+  }
+
+  return 0;
+}
+
 int user_setup(int argc, char *argv[]) {
   bool force_setup = false;
 
-  string at, rt;
+  std::string at, rt;
   bool user_exists = load_tokens(at, rt);
   ac.set_tokens(at, rt);
 
@@ -213,7 +281,7 @@ int user_setup(int argc, char *argv[]) {
 }
 
 void print_help() {
-  cout << "HELP TEXT" << endl;
+  Logger::info << "HELP TEXT" << Logger::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -222,14 +290,21 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  init_autolab_client(ac);
+
   // determine what command it is
   std::string command(argv[1]);
   if ("setup" == command) {
     return user_setup(argc, argv);
   } else if ("download" == command) {
     return download_asmt(argc, argv);
-  } else if ("submit") {
+  } else if ("submit" == command) {
     return submit_asmt(argc, argv);
+  } else if ("courses" == command) {
+    return show_courses(argc, argv);
+  } else if ("assessments" == command ||
+             "asmts" == command) {
+    return show_assessments(argc, argv);
   } else {
     Logger::fatal << "Unrecognized command: " << command << Logger::endl;
   }
