35becafbb94c13e7d8e4d17ef2494b554ac8bc4c
Billy Zhu <yuxiangz@andrew.cmu.edu>
2018-01-21 12:50:32 -0500

    allow option arg parsing in cmdargs

--- src/cmdargs.cpp
+++ src/cmdargs.cpp
@@ -6,16 +6,28 @@
  */
 bool parse_cmdargs(cmdargs &cmd, int argc, char *argv[]) {
   bool args_done = false;
+  char *curr = nullptr;
+  char *next = nullptr;
 
   for (int i = 0; i < argc; i++) {
-    char *curr = argv[i];
+    curr = argv[i];
 
     if (curr[0] == '-') {
       args_done = true;
-      cmd.opts.emplace_back(curr);
+
+      cmd.opts.emplace_back(curr, "");
+      // check if next arg is a value
+      if (i < argc - 1) {
+        next = argv[i+1];
+        if (next[0] != '-') {
+          cmd.opts.back().second = std::string(next);
+          i++;
+        }
+      }
     } else {
       // non-opt arg
       if (args_done) return false;
+
       cmd.args.emplace_back(curr);
     }
   }
--- src/cmdargs.h
+++ src/cmdargs.h
@@ -3,17 +3,30 @@
 
 #include <vector>
 #include <string>
+#include <utility> // pair
 
 struct cmdargs {
+  // matches option name/key to option value
+  // if the option doesn't have a value, it will be an empty string
+  typedef std::pair<std::string, std::string> opt_pair;
+
   std::vector<std::string> args; // positional arguments
-  std::vector<std::string> opts; // options
+  std::vector<opt_pair> opts; // options
 
   bool has_option(std::string name, std::string alt_name = "") {
     for (auto &opt : opts) {
-      if (name == opt || alt_name == opt) return true;
+      if (name == opt.first || alt_name == opt.first) return true;
     }
     return false;
   }
+
+  std::string get_option(std::string name, std::string alt_name = "") {
+    for (auto &opt : opts) {
+      if (name == opt.first || alt_name == opt.first) return opt.second;
+    }
+    return std::string();
+  }
+
   int nargs() {
     return args.size();
   }
