8ca9cd35579552f2b1afb956dfa523d299801ee8
Billy Zhu <yuxiangz@andrew.cmu.edu>
2018-02-10 16:30:32 -0500

    rename libautolab client methods

--- include/autolab/client.h
+++ include/autolab/client.h
@@ -1,38 +1,42 @@
 #ifndef LIBAUTOLAB_CLIENT_H_
 #define LIBAUTOLAB_CLIENT_H_
 
-#include "autolab.h"
-
 #include <string>
 #include <vector>
+
+#include "autolab.h"
+#include "raw_client.h"
 
 namespace Autolab {
 
 class Client {
+private:
+  RawClient raw_client;
+
 public:
   /* setup-related */
   Client(std::string client_id, std::string client_secret, std::string redirect_uri,
          void (*new_token_callback)(std::string, std::string));
-  void SetTokens(std::string access_token, std::string refresh_token);
+  void set_tokens(std::string access_token, std::string refresh_token);
 
   /* oauth-related */
-  void DeviceFlowInit(std::string &user_code, std::string &verification_uri);
-  int DeviceFlowAuthorize(size_t timeout);
+  void device_flow_init(std::string &user_code, std::string &verification_uri);
+  int device_flow_authorize(size_t timeout);
 
   /* resource-related */
-  void GetUserInfo(User &user);
-  void GetCourses(Course &course);
-  void GetAssessments(std::vector<Assessment> &asmts, std::string course_name);
-  void GetAssessmentDetails(DetailedAssessment &asmt, std::string course_name, std::string asmt_name);
-  void GetProblems(std::vector<Problem> &probs, std::string course_name, std::string asmt_name);
-  void GetSubmissions(std::vector<Submission> &subs, std::string course_name, std::string asmt_name);
+  void get_user_info(User &user);
+  void get_courses(std::vector<Course> &courses);
+  void get_assessments(std::vector<Assessment> &asmts, std::string course_name);
+  void get_assessment_details(DetailedAssessment &asmt, std::string course_name, std::string asmt_name);
+  void get_problems(std::vector<Problem> &probs, std::string course_name, std::string asmt_name);
+  void get_submissions(std::vector<Submission> &subs, std::string course_name, std::string asmt_name);
   
   /* action-related */
-  std::string GetFeedback(std::string course_name, std::string asmt_name, int sub_version, std::string problem_name);
-  void DownloadHandout(Attachment &handout, std::string download_dir, std::string course_name, std::string asmt_name);
-  void DownloadWriteup(Attachment &writeup, std::string download_dir, std::string course_name, std::string asmt_name);
+  std::string get_feedback(std::string course_name, std::string asmt_name, int sub_version, std::string problem_name);
+  void download_handout(Attachment &handout, std::string download_dir, std::string course_name, std::string asmt_name);
+  void download_writeup(Attachment &writeup, std::string download_dir, std::string course_name, std::string asmt_name);
   // returns the new submission version number on success
-  int SubmitAssessment(std::string course_name, std::string asmt_name, std::string filename);
+  int submit_assessment(std::string course_name, std::string asmt_name, std::string filename);
 };
 
 }
