5ed9619a414aa632af649468a091335725a9e5d2
TJ <teddyjo@live.com>
2018-04-08 14:48:07 -0400

    Added cachine for 'courses' command

--- src/cache/cache.h
+++ src/cache/cache.h
@@ -3,7 +3,13 @@
 
 
 bool cache_exists();
+
 bool cache_file_exists();
-bool cache_entry_exists();
+
+bool cache_course_entry_exists();
+
+void print_course_cache_entry();
+
+void update_course_cache_entry();
 
 #endif
